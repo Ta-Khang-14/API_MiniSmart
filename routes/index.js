b0dc53a7e26163d9af09f787c36571dacdb64d34
Ta-Khang-14 <vietkhang04@gmail.com>
2021-10-10 22:50:50 +0700

    add home

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const authRouter = require("./auth");
+const homeRouter = require("./home");
 const errHandle = require("../middleware/errorHandle");
 
 const route = (app) => {
     app.use("/api/auth", authRouter);
+    app.use("/", homeRouter);
     app.use(errHandle);
 };
 
