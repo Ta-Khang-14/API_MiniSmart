36a2b2e3740cbe562c960c60c5bf9700002dd6d4
Ta-Khang-14 <vietkhang04@gmail.com>
2021-10-29 09:11:36 +0700

    Update upload file

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -2,12 +2,13 @@ const ErrorResponse = require("../helpers/ErrorResponse");
 const asyncHandle = require("../middleware/asynHandle");
 
 const uploadFile = (req, res, next) => {
-    console.log(req.files);
+    // console.log(req.files);
     if (!req.files) {
         next();
     }
     const arrayUrl = req.files.map((item) => item.path);
-    req.pictures = arrayUrl;
+    req.pictures = [...arrayUrl];
+    // console.log(req.pictures);
     next();
 };
 
