8a4e2d717489057cf8878a70be9452c40dbea3fc
Ta-Khang-14 <vietkhang04@gmail.com>
2021-11-06 07:33:01 +0700

    delete reload route

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,12 @@ const upload = require("./upload");
 const cartRouter = require("./cart");
 
 const route = (app) => {
-    app.use("/api", upload);
-    app.use("/api/auth", authRouter);
-    app.use("/api/categories", categoryRouter);
-    app.use("/api/posts", postRouter);
-    app.use("/api/products", productRouter);
-    app.use("/api/diaries", diaryRouter);
+    // app.use("/api/", upload);
+    app.use("/api/auth/", authRouter);
+    app.use("/api/categories/", categoryRouter);
+    app.use("/api/posts/", postRouter);
+    app.use("/api/products/", productRouter);
+    app.use("/api/diaries/", diaryRouter);
     app.use("/api/cart/", cartRouter);
     app.use("/", homeRouter);
     app.use(errHandle);
