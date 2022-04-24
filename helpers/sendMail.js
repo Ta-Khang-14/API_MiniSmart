2a55032bd4ae857d17b1afcbe5edf991e8be817e
Ta-Khang-14 <vietkhang04@gmail.com>
2022-04-24 12:46:12 +0700

    change token

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,19 +1,41 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
-const { CourierClient } = require("@trycourier/courier");
+const { google } = require("googleapis");
+const { OAuth2 } = google.auth;
 const sendResponse = require("./sendResponse");
+const {
+    GG_CLIENT_ID,
+    GG_CLIENT_SECRET,
+    GG_CLIENT_REFESH_TOKEN,
+    GG_REDIRECT_URL2,
+} = process.env;
 
-const { EMAIL_NAME, PASS_USER, AUTH_TOKEN, EVENT_ID } = process.env;
+const oauth2Client = new OAuth2(
+    GG_CLIENT_ID,
+    GG_CLIENT_SECRET,
+    GG_REDIRECT_URL2
+);
+const { EMAIL_NAME, PASS_USER } = process.env;
 
 const sendUesrMail = async (res, options, data) => {
     try {
+        oauth2Client.setCredentials({
+            refresh_token: GG_CLIENT_REFESH_TOKEN,
+        });
+        const accessToken = await oauth2Client.getAccessToken();
+
         let { email, subject, message, link } = options;
         link = link || "";
 
         const transporter = nodemailer.createTransport({
             service: "Gmail",
             auth: {
+                type: "OAuth2",
                 user: EMAIL_NAME,
+                clientId: GG_CLIENT_ID,
+                clientSecret: GG_CLIENT_SECRET,
+                refreshToken: GG_CLIENT_REFESH_TOKEN,
+                accessToken,
                 pass: PASS_USER,
             },
         });
@@ -36,34 +58,5 @@ const sendUesrMail = async (res, options, data) => {
         });
     }
 };
-
-// const courier = CourierClient({ authorizationToken: AUTH_TOKEN });
-// const sendUesrMail = (res, options, data) => {
-//     let { email, message, link } = options;
-//     courier
-//         .send({
-//             eventId: EVENT_ID, // your Notification ID
-//             profile: {
-//                 email: EMAIL_NAME,
-//             },
-//             message: {
-//                 to: { email },
-//                 template: "SB854GNXN84NK2HJCG0HS4NKEKAE",
-//                 data: {
-//                     recipientName: email,
-//                     contentEmail: message,
-//                     linkConfirm: link,
-//                 },
-//             },
-//         })
-//         .then(() => sendResponse(res, data.message))
-//         .catch((error) => {
-//             console.log(error);
-//             return res.status(500).json({
-//                 message: err.message,
-//                 success: false,
-//             });
-//         });
-// };
 
 module.exports = sendUesrMail;
